60037162942f8243796fec69ba769f37c4d5106f
NaeemAziz1 <77504808+NaeemAziz1@users.noreply.github.com>
2026-08-08 17:43:52 +0600

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["a Programmer", "a Developer", "an IT Expert", "a Designer"],
+        strings: ["a Poet", "an Engineer", "an IT Expert", "a Researcher"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["a Programmer", "a Developer", "an IT Expert", "a Designer"],
+        strings: ["a Poet", "an Engineer", "an IT Expert", "a Researcher"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
