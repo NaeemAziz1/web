4fef8de984a1e2f1ef6385e9f7f1da0467d7e318
NaeemAziz1 <77504808+NaeemAziz1@users.noreply.github.com>
2026-09-16 00:48:50 +0600

    Update script.js

--- script.js
+++ script.js
@@ -695,3 +695,26 @@ m10Image.src = 'images/m10.jpg';
     updatePageUI();
 
 })();
+const extraEducationLightbox =
+    document.getElementById('extraEducationLightbox');
+
+const extraEducationLightboxImage =
+    document.getElementById('extraEducationLightboxImage');
+
+const extraEducationClose =
+    document.getElementById('extraEducationClose');
+
+currentImage.addEventListener('click', function () {
+    extraEducationLightboxImage.src = currentImage.src;
+    extraEducationLightbox.classList.add('active');
+});
+
+extraEducationClose.addEventListener('click', function () {
+    extraEducationLightbox.classList.remove('active');
+});
+
+extraEducationLightbox.addEventListener('click', function (e) {
+    if (e.target === extraEducationLightbox) {
+        extraEducationLightbox.classList.remove('active');
+    }
+});
